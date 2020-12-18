e9dff95bbd4973e61b469a705b57d6e16331b5ac
SeekerNik <nikhilsaini94169@gmail.com>
2020-12-18 22:21:16 +0530

    adding CSS styling

--- src/components/App.js
+++ src/components/App.js
@@ -9,29 +9,42 @@ import ForgotPassword from "./ForgotPassword";
 import Recruiter from "./Recruiter";
 import Candidate from "./Candidate";
 import { Navbar, NavbarBrand, Button } from "react-bootstrap";
+import "../index.css";
 
 function App() {
   return (
-    <div>
-      <Navbar style={{ borderBottom: "3px solid", margin: "0 5%" }}>
-        <NavbarBrand>MyJobs</NavbarBrand>
-        <div style={{ margin: "0 40%" }}></div>
-        <Button className="nav-item px-3" href="/login">
-          Login/Signup
-        </Button>
-      </Navbar>
-      <Router>
-        <AuthProvider>
-          <Switch>
-            <PrivateRoute path="/recruiter" component={Recruiter} />
-            <PrivateRoute path="/candidate" component={Candidate} />
-            <Route path="/" exact component={Home}></Route>
-            <Route path="/signup" component={Signup} />
-            <Route path="/login" component={Login} />
-            <Route path="/forgot-password" component={ForgotPassword} />
-          </Switch>
-        </AuthProvider>
-      </Router>
+    <div class="main">
+      <div class="upper-half">
+        <Navbar
+          style={{ borderBottom: "1px solid", color: "white", margin: "0 5%" }}
+        >
+          <NavbarBrand>
+            <span style={{ color: "white" }}>My</span>
+            <span style={{ color: "#43AFFF" }}>Jobs</span>
+          </NavbarBrand>
+          <div style={{ margin: "0 40%" }}></div>
+          <Button
+            className="nav-item px-3"
+            style={{ backgroundColor: "#506592" }}
+            href="/login"
+          >
+            Login/Signup
+          </Button>
+        </Navbar>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute path="/recruiter" component={Recruiter} />
+              <PrivateRoute path="/candidate" component={Candidate} />
+              <Route path="/" exact component={Home}></Route>
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+      <div class="lower-half"></div>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,57 @@
 import React from "react";
+import { Card, CardDeck } from "react-bootstrap";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <div className="welcome">
+        <h1 style={{ color: "white" }}>Welcome to </h1>
+        <br></br>
+        <h1>
+          <span style={{ color: "white" }}>My</span>
+          <span style={{ color: "#43AFFF" }}>Jobs</span>
+        </h1>
+      </div>
+      <div className="why-us">
+        <h5>Why Us</h5>
+        <CardDeck>
+          <Card>
+            <Card.Body>
+              <Card.Title style={{ color: "#43AFFF", fontWeight: "bold" }}>
+                Card title
+              </Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title style={{ color: "#43AFFF", fontWeight: "bold" }}>
+                Card title
+              </Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to
+                additional content.{" "}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title style={{ color: "#43AFFF", fontWeight: "bold" }}>
+                Card title
+              </Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This card has even longer content
+                than the first to show that equal height action.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardDeck>
+      </div>
     </div>
   );
 }
